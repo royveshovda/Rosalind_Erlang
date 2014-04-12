1a2d842909f0e3502c36e5675492f1bbb43281f5
Roy Veshovda <roy@veshovda.no>
2014-04-12 22:51:55 +0200

    iprb

--- src/iprb.erl
+++ src/iprb.erl
@@ -2,7 +2,9 @@
 -export([prob/3, test/0]).
 
 test()->
-	prob(2, 2, 2).
+	prob(20, 30, 20).
 
 prob(K, M, N) ->
-	ok.
+	Sum = K + M + N,
+    Prob = (N * (N - 1) + N * M + M * (M - 1) / 4.0),
+    (1 - 1 / Sum / (Sum - 1) * Prob).
