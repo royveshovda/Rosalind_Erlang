4ee98d0e0ba8ac627a37eae674ad9d96f9719375
Roy Veshovda <roy@veshovda.no>
2014-04-13 21:20:41 +0200

    PERM

--- src/perm.erl
+++ src/perm.erl
@@ -5,4 +5,21 @@ test() ->
 	generate(3).
 
 generate(N) ->
+	BaseList = get_list_of_all_numbers(N),
+	Perm = generate(BaseList, []).
+
+generate([],Perm) ->
+	Perm;
+generate(List, Perm) ->
+	Length = length(List).
+
+generate(List, Position, Length, Perm) ->
 	ok.
+
+get_list_of_all_numbers(N) ->
+	get_list_of_all_numbers(N, []).
+
+get_list_of_all_numbers(0,List) ->
+	List;
+get_list_of_all_numbers(N, List) ->
+	get_list_of_all_numbers(N-1, [N | List]).
