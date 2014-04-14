16584bff98c46b5afde1f820639ca98afb744142
Roy Veshovda <roy@veshovda.no>
2014-04-14 05:23:15 +0200

    SIGN

--- src/sign.erl
+++ src/sign.erl
@@ -3,9 +3,26 @@
 
 
 test() ->
-	Perms = generate(2),
-	perm:print_perms(Perms).
+	generate_singed_from_list([1,2], []).
+	%Perms = generate(2),
+	%perm:print_perms(Perms).
 
 generate(N) ->
 	%TODO: Create variations with signs
-	perm:generate(N).
+	Perms = perm:generate(N),
+	generate_singed_from_array(Perms, []).
+
+
+generate_singed_from_array([], Acc) ->
+	Acc;
+generate_singed_from_array([Head|Tail], Acc) ->
+	ok.
+
+
+generate_singed_from_list([], Acc) ->
+	Acc;
+generate_singed_from_list([Head|Tail], Acc) ->
+	Tail_as_signed = generate_singed_from_list(Tail, Acc),
+	Acc1 = [Head|Tail_as_signed],
+	Acc2 = [-Head|Tail_as_signed],
+	Acc1 ++ Acc2.
