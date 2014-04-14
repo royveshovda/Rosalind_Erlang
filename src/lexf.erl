b944a76cbd250c36bfd7d4ec3b4ed511c21d91bb
Roy Veshovda <roy@veshovda.no>
2014-04-14 16:26:21 +0200

    LEXF started

---
+++ src/lexf.erl
@@ -0,0 +1,8 @@
+-module(lexf).
+-export([generate/1, test/0]).
+
+test() ->
+	ok.
+
+generate(_X) ->
+	ok.
--- src/sign.erl
+++ src/sign.erl
@@ -22,8 +22,8 @@ generate_singed_from_list([]) ->
 generate_singed_from_list([Head|[]]) ->
 	[Head, -Head];
 generate_singed_from_list([Head|Tail]) ->
-	%io:format("Head:~p~nTail: ~p ~n~n", [Head, Tail]),
 	Tail_as_signed = generate_singed_from_list(Tail),
+	%TODO: Improvethis, so we do not have to call lists_flatten
 	[[H,T] || H <- [Head, -Head], T <- Tail_as_signed ].
 
 flatten_perms([], Acc) ->
