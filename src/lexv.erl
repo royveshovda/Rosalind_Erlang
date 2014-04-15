68932b8b155cc4408d92777affa559e2631164cf
Roy Veshovda <roy@veshovda.no>
2014-04-15 07:02:15 +0200

    LEXV

--- src/lexv.erl
+++ src/lexv.erl
@@ -2,11 +2,9 @@
 -export([generate/2, test/0]).
 
 test() ->
-	%TODO: Implement
-
-	%Result = generate(["T","A","G","C"], 2),
-	Result = generate(["D","Z","P","K", "W", "M"], 3),
-	print(Result).
+	Result = generate(["Ø","D","N","A"], 3),
+	Result.
+	%print(Result).
 
 generate(Lex, Length) ->
 	generate(Lex,Length, Lex).
@@ -14,7 +12,7 @@ generate(Lex, Length) ->
 generate(_Lex, 1, Acc) ->
 	Acc;
 generate(Lex, Length, Acc) ->
-	New_Acc = [[Acc1|Lex1] || Acc1 <- Acc, Lex1 <- Lex],
+	New_Acc = [lists:flatten([Acc1|Lex1]) || Acc1 <- Acc, Lex1 <- Lex],
 	generate(Lex, Length -1, New_Acc).
 
 print([]) ->
--- src/sign.erl
+++ src/sign.erl
@@ -1,11 +1,10 @@
 -module(sign).
--export([generate/1, test/0]).
+-export([generate/1, flatten_perms/1, test/0]).
 
 
 test() ->
 	Perms = generate(3),
-	Flat_Perms = flatten_perms(Perms, []),
-	perm:print_perms(Flat_Perms).
+	perm:print_perms(Perms).
 
 generate(N) ->
 	Perms = perm:generate(N),
@@ -23,11 +22,4 @@ generate_singed_from_list([Head|[]]) ->
 	[Head, -Head];
 generate_singed_from_list([Head|Tail]) ->
 	Tail_as_signed = generate_singed_from_list(Tail),
-	%TODO: Improvethis, so we do not have to call lists_flatten
-	[[H,T] || H <- [Head, -Head], T <- Tail_as_signed ].
-
-flatten_perms([], Acc) ->
-	Acc;
-flatten_perms([Head|Tail],Acc) ->
-	Flat = lists:flatten(Head),
-	flatten_perms(Tail, [Flat|Acc]).
+	[lists:flatten([H,T]) || H <- [Head, -Head], T <- Tail_as_signed ].
