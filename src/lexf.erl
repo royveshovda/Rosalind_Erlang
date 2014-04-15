05901b066a3c71cd4d5e00584673489ca588f979
Roy Veshovda <roy@veshovda.no>
2014-04-15 05:38:23 +0200

    LEXF completed

--- src/lexf.erl
+++ src/lexf.erl
@@ -1,8 +1,22 @@
 -module(lexf).
--export([generate/1, test/0]).
+-export([generate/2, test/0]).
 
 test() ->
-	ok.
+	%Result = generate(["T","A","G","C"], 2),
+	Result = generate(["D","Z","P","K", "W", "M"], 3),
+	print(Result).
 
-generate(_X) ->
-	ok.
+generate(Lex, Length) ->
+	generate(Lex,Length, Lex).
+
+generate(_Lex, 1, Acc) ->
+	Acc;
+generate(Lex, Length, Acc) ->
+	New_Acc = [[Acc1|Lex1] || Acc1 <- Acc, Lex1 <- Lex],
+	generate(Lex, Length -1, New_Acc).
+
+print([]) ->
+	ok;
+print([Head|Tail]) ->
+	io:format("~s~n", [Head]),
+	print(Tail).
