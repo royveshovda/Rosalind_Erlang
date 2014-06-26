ade2c845f08181156ae3492b122f2ab168dd4825
Roy Veshovda <roy@veshovda.no>
2014-06-26 04:54:11 +0200

    lcsm

--- src/lcsm.erl
+++ src/lcsm.erl
@@ -13,18 +13,41 @@ test2() ->
 	%generate_substrings([1,2,3,4]).
 	Filename = "../testdata/rosalind_lcsm.txt",
 	Dnas = basic:open_fasta_file(Filename),
-	[{_Id1, Dna1}|Rest] = Dnas,
-	[{_Id2, Dna2}|_Rest] = Rest,
 
-	Potentials = generate_substrings(Dna1),
-	io:format("Potentials: ~p~n", [length(Potentials)]),
+	Lists = get_dna_lists(Dnas),
+	find_all_sublists(Lists).
 
-	Filtered = basic:remove_dups(Potentials),
-	io:format("Filtered: ~p~n", [length(Filtered)]).
-	%.
 
-	%find_all_common_with_one_dna(Dna2, Potentials).
+get_dna_lists(Dnas) ->
+	get_dna_lists(Dnas, []).
 
+get_dna_lists([], Acc) ->
+	Acc;
+get_dna_lists([{_Id, Dna}|Rest], Acc) ->
+	get_dna_lists(Rest, [Dna|Acc]).
+
+
+find_all_sublists([Dna1 | RestOfDnas]) ->
+	Longest = process_batch(Dna1, RestOfDnas, []),
+
+	Longest.
+process_batch([], _RestOfDnas, Longest) ->
+	Longest;
+process_batch([_Head|[]], _RestOfDnas, Longest) ->
+	Longest;
+process_batch([Head|Tail], RestOfDnas, Longest) ->
+	Potentials = generate_combinations(Head, Tail, [], Longest),
+	
+	CommonPotentialsStrings = find_all_common_with_all_dnas(RestOfDnas, Potentials),
+	NewLongestCandidate = find_longest_list(CommonPotentialsStrings),
+	NewLongest = get_longest_list(Longest, NewLongestCandidate),
+	io:format("~p~n", [NewLongest]),
+	process_batch(Tail, RestOfDnas, NewLongest).
+
+get_longest_list(L1, L2) when length(L1) >= length(L2) ->
+	L1;
+get_longest_list(L1, L2) when length(L1) < length(L2) ->
+	L2.
 
 test() ->
 	Filename = "../testdata/rosalind_lcsm.txt",
@@ -41,15 +64,21 @@ generate_substrings(List) ->
 generate_substrings([], SubLists) ->
 	SubLists;
 generate_substrings([Head|Tail], SubLists) ->
-	NewSubLists = generate_combinations(Head, Tail, []),
+	NewSubLists = generate_combinations(Head, Tail, [], []),
 	generate_substrings(Tail, NewSubLists ++ SubLists).
 
-generate_combinations(Head, [], Combinations) ->
+
+generate_combinations(Head, [], Combinations, _Longest) ->
 	[[Head]|Combinations];
-generate_combinations(Head, Tail, Combinations) ->
+generate_combinations(Head, Tail, Combinations, Longest) ->
 	NewCombinations = [[Head|Tail]|Combinations],
 	NewTail = cut_last_element(Tail),
-	generate_combinations(Head, NewTail, NewCombinations).
+	case length(NewTail) >= length(Longest) of
+		true ->
+			generate_combinations(Head, NewTail, NewCombinations, Longest);
+		false ->
+			Combinations
+	end.
 
 cut_last_element(List) when length(List) =< 1 ->
 	[];
@@ -61,13 +90,12 @@ cut_last_element(List) ->
 find_longest_common(Dnas, Potentials) ->
 	CommonStrings = find_all_common_with_all_dnas(Dnas, Potentials),
 	find_longest_list(CommonStrings).
-	%TODO: Find longest in list
 
 find_all_common_with_all_dnas([], Potentials) ->
 	lists:reverse(Potentials);
-find_all_common_with_all_dnas([{_Id, Dna} | RestOfDna], Potentials) ->
+find_all_common_with_all_dnas([Dna | RestOfDnas], Potentials) ->
 	NewPotentials = find_all_common_with_one_dna(Dna, Potentials),
-	find_all_common_with_all_dnas(RestOfDna, NewPotentials).
+	find_all_common_with_all_dnas(RestOfDnas, NewPotentials).
 
 find_all_common_with_one_dna(Dna, Potentials) ->
 	find_all_common_with_one_dna(Dna, Potentials, []).
@@ -75,13 +103,18 @@ find_all_common_with_one_dna(Dna, Potentials) ->
 find_all_common_with_one_dna(_Dna, [], Acc) ->
 	Acc;
 find_all_common_with_one_dna(Dna, [Candidate|RestOfTheCandidates], Acc) ->
-	io:format("~p~n", [length(RestOfTheCandidates)]),
+	print_length(length(RestOfTheCandidates)),
 	case contains(Dna, Candidate) of
 		false ->
 			find_all_common_with_one_dna(Dna, RestOfTheCandidates, Acc);
 		true ->
 			find_all_common_with_one_dna(Dna, RestOfTheCandidates, [Candidate|Acc])
 	end.
+
+print_length(Length) when Length rem 100 =:= 0 ->
+	io:format("~p~n", [Length]);
+print_length(_Length) ->
+	void.
 
 
 find_longest_list(Lists) ->
@@ -94,13 +127,6 @@ find_longest_list([Candidate | Rest], Longest) when length(Candidate) > length(L
 find_longest_list([Candidate | Rest], Longest) when length(Candidate) =< length(Longest) ->
 	find_longest_list(Rest, Longest).
 
-% expand the first string, and get all possible substrings
-
-% use all the possible substrings, and go through them one by one.
-% search each og the other strings for motif (subs:motif)
-% save candidates along the way
-
-% compare all the candidates, and return the longest
 
 
 contains(List, Match) ->
