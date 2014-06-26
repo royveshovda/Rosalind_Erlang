d148bd163431066e06a5f0cd98bd41867ad102aa
Roy Veshovda <roy@veshovda.no>
2014-06-26 06:18:57 +0200

    lcsm

--- src/lcsm.erl
+++ src/lcsm.erl
@@ -1,5 +1,5 @@
 -module(lcsm).
--export([test/0, test2/0, test3/0]).
+-export([test/0, test3/0, test4/0, test5/0, process_batch/4]).
 
 test3() ->
 	List = [1,2,3,4,5,6],
@@ -9,14 +9,63 @@ test3() ->
 	false = contains(List, Match2),
 	perfect.
 
-test2() ->
-	%generate_substrings([1,2,3,4]).
+test() ->
+	B = [1,2,3,4,5],
+	generate_combinations(B).
+
+test5() ->
+	B = [[1,2,3,4,5]],
+	generate_batches(B).
+
+test4() ->
 	Filename = "../testdata/rosalind_lcsm.txt",
-	Dnas = basic:open_fasta_file(Filename),
+	Dnas = basic:open_fasta_file(Filename),	
 
 	Lists = get_dna_lists(Dnas),
-	find_all_sublists(Lists).
+	[Dna1|RestOfDnas] = Lists,
 
+	Batches = generate_batches(Dna1),
+
+	create_workers(lists:reverse(Batches), RestOfDnas, 0),
+
+	process_responses(length(Batches)).
+
+create_workers([], _RestOfDnas, _SerialNumber) ->
+	ok;
+create_workers([Batch1|Rest], RestOfDnas, SerialNumber) ->
+	spawn(?MODULE, process_batch, [Batch1, RestOfDnas, self(), SerialNumber]),
+	create_workers(Rest, RestOfDnas, SerialNumber+1).
+
+
+process_responses(NumbersToReceive) ->
+	process_responses(NumbersToReceive, 0, []).
+
+process_responses(NumbersToReceive, Received, Longest) when NumbersToReceive == Received ->
+	Longest;
+process_responses(NumbersToReceive, Received, Longest) ->
+	receive
+		{response, _SerialNumber, LongestCandidate} ->
+			io:format("Received: ~p~n", [Received+1]),
+			NewLongest = get_longest_list(Longest, LongestCandidate),
+			process_responses(NumbersToReceive, Received+1, NewLongest)
+	end.
+
+
+
+process_batch(Dna, RestOfDnas, ResponsePid, SerialNumber) ->
+	Potentials = generate_combinations(Dna),	
+	CommonPotentialsStrings = find_all_common_with_all_dnas(RestOfDnas, Potentials),
+	Longest = find_longest_list(CommonPotentialsStrings),
+	ResponsePid ! {response, SerialNumber, Longest}.
+
+generate_batches(Dna1) ->
+	generate_batches(Dna1, []).
+
+
+generate_batches([], SubLists) ->
+	SubLists;
+generate_batches([Head|Tail], SubLists) ->
+	generate_batches(Tail, [[Head|Tail]|SubLists]).
 
 get_dna_lists(Dnas) ->
 	get_dna_lists(Dnas, []).
@@ -26,70 +75,26 @@ get_dna_lists([], Acc) ->
 get_dna_lists([{_Id, Dna}|Rest], Acc) ->
 	get_dna_lists(Rest, [Dna|Acc]).
 
-
-find_all_sublists([Dna1 | RestOfDnas]) ->
-	Longest = process_batch(Dna1, RestOfDnas, []),
-
-	Longest.
-process_batch([], _RestOfDnas, Longest) ->
-	Longest;
-process_batch([_Head|[]], _RestOfDnas, Longest) ->
-	Longest;
-process_batch([Head|Tail], RestOfDnas, Longest) ->
-	Potentials = generate_combinations(Head, Tail, [], Longest),
-	
-	CommonPotentialsStrings = find_all_common_with_all_dnas(RestOfDnas, Potentials),
-	NewLongestCandidate = find_longest_list(CommonPotentialsStrings),
-	NewLongest = get_longest_list(Longest, NewLongestCandidate),
-	io:format("~p~n", [NewLongest]),
-	process_batch(Tail, RestOfDnas, NewLongest).
-
 get_longest_list(L1, L2) when length(L1) >= length(L2) ->
 	L1;
 get_longest_list(L1, L2) when length(L1) < length(L2) ->
 	L2.
 
-test() ->
-	Filename = "../testdata/rosalind_lcsm.txt",
-	Dnas = basic:open_fasta_file(Filename),
-	[{_Id1, Dna1}|Rest] = Dnas,
-	Potentials = generate_substrings(Dna1),
-	%length(Potentials).
-	Longest = find_longest_common(Rest, Potentials),
-	basic:dna_to_string(Longest).
+generate_combinations([Head|Tail]) ->
+	generate_combinations(Head, Tail, []).
 
-generate_substrings(List) ->
-	generate_substrings(List, []).
-
-generate_substrings([], SubLists) ->
-	SubLists;
-generate_substrings([Head|Tail], SubLists) ->
-	NewSubLists = generate_combinations(Head, Tail, [], []),
-	generate_substrings(Tail, NewSubLists ++ SubLists).
-
-
-generate_combinations(Head, [], Combinations, _Longest) ->
+generate_combinations(Head, [], Combinations) ->
 	[[Head]|Combinations];
-generate_combinations(Head, Tail, Combinations, Longest) ->
+generate_combinations(Head, Tail, Combinations) ->
 	NewCombinations = [[Head|Tail]|Combinations],
 	NewTail = cut_last_element(Tail),
-	case length(NewTail) >= length(Longest) of
-		true ->
-			generate_combinations(Head, NewTail, NewCombinations, Longest);
-		false ->
-			Combinations
-	end.
+	generate_combinations(Head, NewTail, NewCombinations).
 
 cut_last_element(List) when length(List) =< 1 ->
 	[];
 cut_last_element(List) ->
 	Length = length(List),
 	lists:sublist(List,1,Length-1).
-
-
-find_longest_common(Dnas, Potentials) ->
-	CommonStrings = find_all_common_with_all_dnas(Dnas, Potentials),
-	find_longest_list(CommonStrings).
 
 find_all_common_with_all_dnas([], Potentials) ->
 	lists:reverse(Potentials);
@@ -103,7 +108,7 @@ find_all_common_with_one_dna(Dna, Potentials) ->
 find_all_common_with_one_dna(_Dna, [], Acc) ->
 	Acc;
 find_all_common_with_one_dna(Dna, [Candidate|RestOfTheCandidates], Acc) ->
-	print_length(length(RestOfTheCandidates)),
+	%print_length(length(RestOfTheCandidates)),
 	case contains(Dna, Candidate) of
 		false ->
 			find_all_common_with_one_dna(Dna, RestOfTheCandidates, Acc);
