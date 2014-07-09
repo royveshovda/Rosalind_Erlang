a41d8f214a0b7438f82539e39a4436a12c489aff
Roy Veshovda <roy@veshovda.no>
2014-07-09 17:14:23 +0200

    lgis continued

--- src/lgis.erl
+++ src/lgis.erl
@@ -1,5 +1,5 @@
 -module(lgis).
--export([test/0]).
+-export([test/0, get_longest_increasing_subsequence/1]).
 
 
 test() ->
@@ -12,7 +12,14 @@ test() ->
 %	Decr = get_longest_decreasing_subsequence(Permutaion)
 
 get_longest_increasing_subsequence(List) ->
-	List.
+	get_longest_increasing_subsequence(List, 0, []).
+
+get_longest_increasing_subsequence([], _LatestAddedToSequence, Result) ->
+	lists:reverse(Result);
+get_longest_increasing_subsequence([Head|Tail], LatestAddedToSequence, Result) when Head > LatestAddedToSequence ->
+	get_longest_increasing_subsequence(Tail, Head, [Head, Result]);
+get_longest_increasing_subsequence([_Head|Tail], LatestAddedToSequence, Result) when _Head =< LatestAddedToSequence ->
+	get_longest_increasing_subsequence(Tail, LatestAddedToSequence, Result).
 
 
 %1,3,4
