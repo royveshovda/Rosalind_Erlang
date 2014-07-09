294a3787b7e2139fe18d32f685d6337c6fe52958
Roy Veshovda <roy@veshovda.no>
2014-07-09 20:16:13 +0200

    lgis continued

--- src/lgis.erl
+++ src/lgis.erl
@@ -3,25 +3,51 @@
 
 
 test() ->
-	Permutation1 = [1],
-	[1] = get_longest_increasing_subsequence(Permutation1),
+	Perm1 = [1],
+	[1] = get_longest_increasing_subsequence(Perm1),
 
-	Permutation2 = [1,3,4,2],
-	[1,3,4] = get_longest_increasing_subsequence(Permutation2),
+	Perm2 = [1,2,3],
+	[1,2,3] = get_longest_increasing_subsequence(Perm2),
+	perfect,
+
+	Perm3 = [1,2,3,2],
+	[1,2,3] = get_longest_increasing_subsequence(Perm3),
+
+	Perm4 = [1,2,1,3,4],
+	[1,2,3,4] = get_longest_increasing_subsequence(Perm4),
+
+	Perm5 = [1,4,2,3],
+	[1,2,3] = get_longest_increasing_subsequence(Perm5),
+
 	perfect.
-%	Decr = get_longest_decreasing_subsequence(Permutaion)
 
 get_longest_increasing_subsequence(List) ->
-	get_longest_increasing_subsequence(List, 0, []).
+	get_longest_increasing_subsequence(List, []).
 
-get_longest_increasing_subsequence([], _LatestAddedToSequence, Result) ->
+get_longest_increasing_subsequence([], Result) ->
 	lists:reverse(Result);
-get_longest_increasing_subsequence([Head|Tail], LatestAddedToSequence, Result) when Head > LatestAddedToSequence ->
-	get_longest_increasing_subsequence(Tail, Head, [Head, Result]);
-get_longest_increasing_subsequence([_Head|Tail], LatestAddedToSequence, Result) when _Head =< LatestAddedToSequence ->
-	get_longest_increasing_subsequence(Tail, LatestAddedToSequence, Result).
+get_longest_increasing_subsequence([Head|Tail], []) ->
+	get_longest_increasing_subsequence(Tail,[Head]);
 
+get_longest_increasing_subsequence([Head1,Head2|Tail],[RHead|_RTail] = Result) when Head2 > Head1, Head1 > RHead ->
+	get_longest_increasing_subsequence([Head2|Tail], [Head1|Result]);
 
+get_longest_increasing_subsequence([Head1,Head2|Tail],[RHead|_RTail] = Result) when Head2 =< Head1, Head1 > RHead ->
+	Cand1 = get_longest_increasing_subsequence(Tail, [Head1|Result]),
+	Cand2 = get_longest_increasing_subsequence(Tail, [Head2|Result]),
+	
+	L1 = length(Cand1),
+	L2 = length(Cand2),
+
+	case L1 >= L2 of
+		true ->
+			Cand1;
+		false ->
+			Cand2
+	end;
+
+	get_longest_increasing_subsequence([_Head|Tail],[_RHead|_RTail] = Result) when _Head =< _RHead ->
+	get_longest_increasing_subsequence(Tail, Result).
 %1,3,4
 %1,3
 %1,4
