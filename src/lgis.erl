3c84ac32e626a7da7fbf29d04c30d302515b1344
Roy Veshovda <roy@veshovda.no>
2014-07-16 07:02:17 +0200

    lgis ready for submit

--- src/lgis.erl
+++ src/lgis.erl
@@ -1,6 +1,14 @@
 -module(lgis).
--export([test/0, test2/0]).
+-export([test/0, run/0]).
 
+run()->
+	PermAsString = "5 1 4 2 3",
+	Perm = string_to_list(PermAsString),
+	{Incr, Decr} = get_longest_subsequences(Perm),
+	io:format("~n~nIncreasing: ~n"),
+	print_list(Incr),
+	io:format("~n~nDecreasing:~n"),
+	print_list(Decr).
 
 test() ->
 	Perm1 = [1],
@@ -24,18 +32,14 @@ test() ->
 
 	Perm7 = [5,1,4,2,3],
 	[1,2,3] = get_longest_increasing_subsequence(Perm7),
-	%[5,4,2] = get_longest_decreasing_subsequence(Perm7),
+	[5,4,2] = get_longest_decreasing_subsequence(Perm7),
 
 	Perm8 = [8,2,1,6,5,7,4,3,9],
 	[2,6,7,9] = get_longest_increasing_subsequence(Perm8),
-	%[8,6,5,4,3] = get_longest_decreasing_subsequence(Perm8),
-	
+	[8,6,5,4,3] = get_longest_decreasing_subsequence(Perm8),
 
-	%BUG: Must be fixed to check all sub-sequences, if they are longer, not only the two first candidates
 	Perm9 = [2,8,9,4,5,3,6,7,1],
 	[2,4,5,6,7] = get_longest_increasing_subsequence(Perm9),
-
-	
 
 	String1 = "5 1 4 2 3",
 	[5,1,4,2,3] = string_to_list(String1),
@@ -45,63 +49,52 @@ test() ->
 
 	perfect.
 
-test2()->
-	Perm = [2,8,9,4,5,3,6,7,1],
-	[2,4,5,6,7] = get_longest_increasing_subsequence(Perm),
-	perfect.
-
+get_longest_subsequences(List) ->
+	Incr = get_longest_increasing_subsequence(List),
+	Decr = get_longest_decreasing_subsequence(List),
+	{Incr, Decr}.
 
 get_longest_increasing_subsequence(List) ->
-	get_longest_increasing_subsequence(List, []).
+	Comparer = fun(X,Y) -> X > Y end,
+	get_longest(List, Comparer).
 
-get_longest_increasing_subsequence([], Result)->
-	Result;
-get_longest_increasing_subsequence([_Head|Tail] = List, Result) ->
-	Candidate = get_longest_increasing_subsequence2(List),
-	case length(Candidate) > length(Result) of
-		true ->
-			get_longest_increasing_subsequence(Tail, Candidate);
-		false ->
-			get_longest_increasing_subsequence(Tail, Result)
-	end.
-
-
-get_longest_increasing_subsequence2(List) ->
-	get_longest_increasing_subsequence2(List, []).
-
-get_longest_increasing_subsequence2([], Result) ->
-	lists:reverse(Result);
-get_longest_increasing_subsequence2([Head1,Head2|Tail],[RHead|_RTail] = Result) when Head2 > Head1, Head1 > RHead ->
-	get_longest_increasing_subsequence2([Head2|Tail], [Head1|Result]);
-get_longest_increasing_subsequence2([Head1,Head2|Tail],[RHead|_RTail] = Result) when Head2 =< Head1, Head1 > RHead ->
-	Cand1 = get_longest_increasing_subsequence2(Tail, [Head1|Result]),
-	Cand2 = get_longest_increasing_subsequence2(Tail, [Head2|Result]),
-	L1 = length(Cand1),
-	L2 = length(Cand2),
-
-	case L1 >= L2 of
-		true ->
-			Cand1;
-		false ->
-			Cand2
-	end;
-get_longest_increasing_subsequence2([Head|Tail], []) ->
-	get_longest_increasing_subsequence2(Tail,[Head]);
-get_longest_increasing_subsequence2([Head|Tail], [RHead|_RTail] = Result) when Head > RHead ->
-	get_longest_increasing_subsequence2(Tail, [Head|Result]);
-get_longest_increasing_subsequence2([_Head|Tail], [_RHead|_RTail] = Result) when _Head =< _RHead ->
-	get_longest_increasing_subsequence2(Tail, Result);
-get_longest_increasing_subsequence2([_Head|Tail],[_RHead|_RTail] = Result) when _Head =< _RHead ->
-	get_longest_increasing_subsequence2(Tail, Result).
-
+get_longest_decreasing_subsequence(List) ->
+	Comparer = fun(X,Y) -> X < Y end,
+	get_longest(List, Comparer).
 
 string_to_list(String) ->
 	lists:map(fun(X) -> {Int, _} = string:to_integer(X), 
                     Int end, 
           string:tokens(String, " ")).
 
-%print_list([]) ->
-%	io:format("~n");
-%print_list([Head|Tail]) ->
-%	io:format("~p ", [Head]),
-%	print_list(Tail).
+print_list([]) ->
+	io:format("~n");
+print_list([Head|Tail]) ->
+	io:format("~p ", [Head]),
+	print_list(Tail).
+
+get_longest(List, Comparer) ->
+	lists:reverse(get_longest(List, Comparer, [])).
+
+get_longest([], _Comparer, Longest) ->
+	Longest;
+get_longest([Head|Tail], Comparer, []) ->
+	Candidate1 = get_longest(Tail, Comparer, []),
+	Candidate2 = get_longest(Tail, Comparer, [Head]),
+	longest(Candidate1, Candidate2);
+get_longest([Head|Tail], Comparer, [LHead| _LTail] = Longest) ->
+	Candidate1 = get_longest(Tail, Comparer, Longest),
+	NewLongest1 = longest(Longest, Candidate1),
+
+	case Comparer(Head, LHead) of
+		true ->
+			Candidate2 = get_longest(Tail, Comparer, [Head|Longest]),
+			longest(NewLongest1, Candidate2);
+		false ->
+			NewLongest1
+	end.
+
+longest(Candidate1, _Candidate2) when length(Candidate1) > length(_Candidate2) ->
+	Candidate1;
+longest(_Candidate1, Candidate2) when length(_Candidate1) =< length(Candidate2) ->
+	Candidate2.
